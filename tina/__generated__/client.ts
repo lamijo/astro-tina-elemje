5df9548e2c430d28c4ab432cfe1638a8979f9957
Lamijo <lamijo@smamas.sch.id>
2023-04-11 04:41:29 +0700

    Add typescript dev dependency

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/ac1ae0b1-8643-4ffd-87f8-283105ba7694/github/main', token: '8bae999b79378f1badd8193a170b5805847c9c60', queries });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'null', queries });
 export default client;
   
