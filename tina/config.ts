3decee9d2847a28a24c834f024982bc7232c32d9
Lamijo <lamijo@smamas.sch.id>
2023-04-10 20:12:25 +0700

    Add github action CI to deploy with Github Pages

--- tina/config.ts
+++ tina/config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const clientId = process.env.TINA_CLIENTID || null;
+const token = process.env.TINA_TOKEN || null;
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId,
+  token,
 
   build: {
     outputFolder: "admin",
